f17c5b6fdbc26e669b077201da0392cd380a982f
Mark Artishuk <markyshuk@gmail.com>
2021-05-01 23:03:32 -0700

    Base commit, working step functionality

---
+++ script.js
@@ -0,0 +1,35 @@
+let currentStep = 0;
+
+function setStep(stepNumber) {
+  const steps = document.querySelectorAll(".step");
+
+  if (stepNumber >= steps.length) return;
+
+  // Hide all other steps
+  steps.forEach((stepElem, index) => {
+    if (index !== stepNumber) stepElem.style.display = "none";
+  });
+
+  // show step
+  steps[stepNumber].style.display = "block";
+}
+
+function nextStep() {
+  currentStep++;
+  setStep(currentStep);
+}
+
+// Register events on all form-buttons
+function registerFormButtons() {
+  const btns = document.querySelectorAll(".form-button");
+
+  btns.forEach((btnElem) => {
+    btnElem.addEventListener("click", nextStep);
+  });
+  for (let i = 0; i < btns.length; i++) {}
+}
+
+window.addEventListener("load", () => {
+  registerFormButtons();
+  setStep(currentStep);
+});

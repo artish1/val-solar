436253ab6889b206bfb8a2235901997e51a18803
Mark Artishuk <markyshuk@gmail.com>
2021-06-04 20:22:17 -0700

    Field requirements added, and changed success submission text

--- script.js
+++ script.js
@@ -148,7 +148,20 @@ function getMaxSteps() {
   return document.querySelectorAll(".step").length;
 }
 
+function registerTextInputEvents() {
+  const addressInput = document.getElementById("address");
+  addressInput.addEventListener("input", updateAddressContinueButton);
+  addressInput.addEventListener("change", updateAddressContinueButton);
+
+  const emailInput = document.getElementById("email");
+  emailInput.addEventListener("input", updateEmailContinueButton);
+  emailInput.addEventListener("change", updateEmailContinueButton);
+}
+
 async function submit() {
+  // Check fields first
+  if (!checkSubmitFields()) return;
+
   const finishedElem = document.querySelector(".finished");
   const loadingIcon = document.querySelector(".loading-icon");
   const success = document.getElementById("success");
@@ -233,13 +246,78 @@ function initMap() {
   };
   const autocomplete = new google.maps.places.Autocomplete(input, options);
   autocomplete.setFields(["place_id", "geometry", "name"]);
+  autocomplete.addListener("place_changed", updateAddressContinueButton);
+}
+
+function updateAddressContinueButton() {
+  const input = document.getElementById("address");
+
+  // get continue button sibling of input
+  const btn = input.parentElement.querySelector(".continue-button");
+
+  // Test input text
+  if (input.value.length >= 7) btn.disabled = false;
+  else btn.disabled = true;
+}
+
+function updateEmailContinueButton() {
+  const input = document.getElementById("email");
+
+  // get continue button sibling of input
+  const btn = input.parentElement.querySelector(".continue-button");
+  // Regex pattern to test email validation
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(input.value)) btn.disabled = false;
+  else btn.disabled = true;
+}
+
+function checkSubmitFields() {
+  let valid = true;
+  const fName = document.getElementById("first-name");
+  const lName = document.getElementById("last-name");
+  const phone = document.getElementById("phone-number");
+
+  // error elements
+  const fNameError = document.getElementById("first-name-error");
+  const lNameError = document.getElementById("last-name-error");
+  const phoneError = document.getElementById("phone-number-error");
+
+  if (fName.value.length === 0) {
+    valid = false;
+    // Show popup
+    show(fNameError);
+  } else hide(fNameError);
+
+  if (lName.value.length === 0) {
+    valid = false;
+    show(lNameError);
+  } else hide(lNameError);
+
+  if (phone.length < 10) {
+    valid = false;
+    // show error popup
+    show(phoneError);
+    phoneError.textContent = "Phone number requires at least 10 digits";
+  } else {
+    const phoneRe = /^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+    if (!phoneRe.test(phone.value)) {
+      valid = false;
+      // show error popup
+      show(phoneError);
+      phoneError.textContent = "Invalid phone number. Example: (111) 111-1111";
+    } else hide(phoneError);
+  }
+
+  return valid;
 }
 
 window.addEventListener("load", () => {
   registerFormButtons();
   registerSliders();
   setStep(currentStep);
-  // hideAllSteps();
+  registerTextInputEvents();
   registerArrowButtons();
   initMap();
   syncBillSlider();
